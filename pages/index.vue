a4e4bf3944687979bfede7660743abec83de9f93
Jungea <jungea2241@gmail.com>
2023-05-27 19:45:16 +0900

    [Assets] public / assets 사용

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Welcome to the homepage</h1>
+    <img src="/img/BKing.png" alt="public image" />
+    <img src="~/assets/img/WQueen.png" alt="public image" />
     <AppAlert>
       This is an auto-imported component
     </AppAlert>
